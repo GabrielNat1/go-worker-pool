5ed57f424f58ffc0c65b814440a241f2f5ddffb3
GabrielNat1 <dcbiel846@gmail.com>
2025-11-08 18:56:53 -0300

    feat(logger): add basic logging utilities

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,18 @@
 package logger
+
+import (
+	"fmt"
+	"time"
+)
+
+func Info(msg string) {
+	fmt.Printf("[INFO] %s | %s\n", time.Now().Format(time.RFC3339), msg)
+}
+
+func Error(msg string) {
+	fmt.Printf("[ERROR] %s | %s\n", time.Now().Format(time.RFC3339), msg)
+}
+
+func Warn(msg string) {
+	fmt.Printf("[WARN] %s | %s\n", time.Now().Format(time.RFC3339), msg)
+}
